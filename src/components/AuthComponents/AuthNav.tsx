94e482665f535f3114f9c7bfd5850cf3c8c97476
John Dev <kingsoorsa@gmail.com>
2025-04-29 15:21:44 +0100

    integrated api with login and register screen v.01

--- src/components/AuthComponents/AuthNav.tsx
+++ src/components/AuthComponents/AuthNav.tsx
@@ -13,7 +13,7 @@ export default function AuthNavbar() {
   ];
 
   return (
-    <header className="relative bottom-0 z-50 w-full transition-colors duration-300">
+    <header className="fixed bottom-0 z-50 w-full transition-colors duration-300">
       <nav className="w-full flex justify-between items-center py-2 px-2 md:px-2">
         <ul className="hidden md:flex space-x-6 text-sm">
           {navLinks.map((link) => (
