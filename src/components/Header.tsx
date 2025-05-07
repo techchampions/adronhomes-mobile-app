f258a4c03ee5e6a982ee7e0d56da87c8f49c41ec
John Dev <kingsoorsa@gmail.com>
2025-05-07 16:43:19 +0100

    created notification & transaction details

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,16 @@ import { FaUser } from "react-icons/fa";
 import InputField from "./InputField";
 import Button from "./Button";
 import { Formik } from "formik";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const newProperty = () => {
+    navigate("/new-properties");
+  };
+  const goTpProfile = () => {
+    navigate("/my-profile");
+  };
   return (
     <div className="hidden md:flex justify-between items-center bg-white rounded-3xl p-8 mb-5">
       <div className="text-2xl">Dashboard</div>
@@ -28,9 +36,14 @@ const Header = () => {
           src="/images/notifications-rounded.svg"
           alt=""
           className="h-7 w-7"
+          onClick={() => navigate("/notifications")}
         />
-        <Button label="New Property" className="bg-adron-green text-sm px-4" />
-        <div className="p-2 bg-adron-body rounded-full">
+        <Button
+          label="New Property"
+          className="bg-adron-green text-sm px-4"
+          onClick={newProperty}
+        />
+        <div className="p-2 bg-adron-body rounded-full" onClick={goTpProfile}>
           <FaUser className="h-4 w-4" />
         </div>
       </div>
