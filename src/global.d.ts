9eeee8b59f53eed03fd27e1283a532f35cef6e9d
John Dev <kingsoorsa@gmail.com>
2025-08-14 11:09:20 +0100

    comment tawk.to module

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,19 +1,19 @@
 declare module "*.css";
 // src/global.d.ts
-export {};
+// export {};
 
-declare global {
-  interface Window {
-    Tawk_API?: {
-      showWidget: () => void;
-      hideWidget: () => void;
-      maximize: () => void;
-      minimize: () => void;
-      toggle: () => void;
-      popup: () => void;
-      onLoad: (callback: () => void) => void;
-      // Add other methods you plan to use
-    };
-    Tawk_LoadStart?: Date;
-  }
-}
+// declare global {
+//   interface Window {
+//     Tawk_API?: {
+//       showWidget: () => void;
+//       hideWidget: () => void;
+//       maximize: () => void;
+//       minimize: () => void;
+//       toggle: () => void;
+//       popup: () => void;
+//       onLoad: (callback: () => void) => void;
+//       // Add other methods you plan to use
+//     };
+//     Tawk_LoadStart?: Date;
+//   }
+// }
