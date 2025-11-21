71353a1672955d72b05bd3479f5fa73cedeac75f
khadii <66786578+khadii@users.noreply.github.com>
2025-11-21 17:23:49 +0100

    Kadiri new work (#26)
    * upddate on app
    
    * update on newly added pages
    
    * update
    
    * purpose
    
    * purpose
    
    * change on new prop card and sort
    
    * updates
    
    * update on date picker
    
    * search and sort
    
    * header
    
    * changes
    
    * Your changes
    
    * update on properties
    
    * header
    
    * image null
    
    * update on notification and transaction
    
    * update on notification and transaction
    
    * share added to reciept button
    
    * update on agreement and discription
    
    * udate on login and settings
    
    * update on agreement and discription
    
    * update on agreement and discription
    
    * update on new-properties
    
    * update
    
    * download receipt update
    
    * update on transactions
    
    * paystack
    
    * update on wallet
    
    * update on wallettrx
    
    * update on image display
    
    * update on sort
    
    * funding using interswitch
    
    * changes
    
    * update on interswitch
    
    * update on interswitch

--- src/components/DashboardHomeComponents/AddFundAmount.tsx
+++ src/components/DashboardHomeComponents/AddFundAmount.tsx
@@ -13,7 +13,7 @@ const AddFundAmount = ({ goBack }: { goBack: () => void }) => {
   const validationSchema = Yup.object().shape({
     amount: Yup.number()
       .typeError("Amount must be a number")
-      .min(1001, "Amount must be greater than ₦1,000")
+      // .min(1001, "Amount must be greater than ₦1,000")
       .required("Amount is required"),
   });
 
