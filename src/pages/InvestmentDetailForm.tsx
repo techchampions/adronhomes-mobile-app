48f5654be10f9586a06f3bc17b2c90ced5dfc3d7
khadii <66786578+khadii@users.noreply.github.com>
2025-12-17 16:51:32 +0100

    Kadiri new work (#34)
    * upddate on app
    
    * update on newly added pages
    
    * update
    
    * purpose
    
    * purpose
    
    * change on new prop card and sort
    
    * updates
    
    * update on date picker
    
    * search and sort
    
    * header
    
    * changes
    
    * Your changes
    
    * update on properties
    
    * header
    
    * image null
    
    * update on notification and transaction
    
    * update on notification and transaction
    
    * share added to reciept button
    
    * update on agreement and discription
    
    * udate on login and settings
    
    * update on agreement and discription
    
    * update on agreement and discription
    
    * update on new-properties
    
    * update
    
    * download receipt update
    
    * update on transactions
    
    * paystack
    
    * update on wallet
    
    * update on wallettrx
    
    * update on image display
    
    * update on sort
    
    * funding using interswitch
    
    * changes
    
    * update on interswitch
    
    * update on interswitch
    
    * update
    
    * receipt
    
    * update on receipt
    
    * inclusion of contracts table and view contract page
    
    * update
    
    * update on contrct and sign in
    
    * update on contrct and sign in
    
    * update on contrct and sign in
    
    * new unauthdashboard
    
    * update on payment and fixes
    
    * ui customer table update
    
    * verion 1 commit
    
    * update
    
    * update

--- src/pages/InvestmentDetailForm.tsx
+++ src/pages/InvestmentDetailForm.tsx
@@ -5,11 +5,19 @@ import { PassportPhotoForm } from "../components/InvestFormSCreenComponents/Pass
 import { CustomerForm } from "../components/InvestFormSCreenComponents/CustomerForm";
 import { BasicInfoForm } from "../components/InvestFormSCreenComponents/BasicInfoForm";
 import { IdInfoForm } from "../components/InvestFormSCreenComponents/IdInfoForm";
+import { useGetPropertyByID } from "../data/hooks";
+import { useParams } from "react-router-dom";
+import SmallLoader from "../components/SmallLoader";
+import { Home, Ticket } from "lucide-react";
 
 const InvestmentDetailForm = () => {
-  // import other tab components...
+  const params = useParams();
+  const id = params?.id;
+
+  const { data, isError, isLoading } = useGetPropertyByID(id || 0);
 
   const [activeTab, setActiveTab] = useState(0);
+  if (isLoading) return <SmallLoader />;
 
   const renderTab = () => {
     switch (activeTab) {
@@ -42,6 +50,11 @@ const InvestmentDetailForm = () => {
 
   return (
     <div className="bg-white rounded-3xl p-6">
+      <div className="text-lg md:text-2xl font-adron-bold flex items-start md:items-center gap-2">
+        <Home size={30} />
+        <div className="">Subscribe to {data?.data.properties.name}</div>
+      </div>
+
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
       {renderTab()}
     </div>
