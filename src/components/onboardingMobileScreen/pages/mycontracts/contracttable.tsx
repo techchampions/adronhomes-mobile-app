f90b435589eea2a728fe848a76abc3f9480c9a4a
khadii <66786578+khadii@users.noreply.github.com>
2026-05-26 10:20:46 +0100

    Kd (#46)
    * update on nrtlify error
    
    * update

--- src/components/onboardingMobileScreen/pages/mycontracts/contracttable.tsx
+++ src/components/onboardingMobileScreen/pages/mycontracts/contracttable.tsx
@@ -144,6 +144,7 @@ export default function ContractsPage() {
                       <th className="px-6 py-4 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         Property Name
                       </th>
+                      
                       <th className="px-6 py-4 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         Property Net Value
                       </th>
